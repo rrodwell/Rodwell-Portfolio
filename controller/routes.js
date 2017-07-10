044abf0a3073e43d0c52310c83e2512ca311e42a
Ryan Rodwell <ryanrodwell@gmail.com>
2017-07-10 14:54:02 -0400

    testing routes/videos

---
+++ controller/routes.js
@@ -0,0 +1,17 @@
+/**
+ * Created by ryanrodwell on 7/10/17.
+ */
+var express = require("express");
+var router = express.Router();
+var path = require("path");
+
+router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/videos/burger_demo", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/videos/burger_demo.mp4"));
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,14 +3,25 @@
  */
 // Include Server Dependencies
 var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+
+var appController = require("./controller/routes");
 
 // Create Instance of Express
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+// Run Morgan for Logging
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(express.static("./public"));
 
+app.use("/", appController);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
