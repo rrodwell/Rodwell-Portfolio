1abd2d94721cecacdc6b0ac81ea0296daea74a17
Ryan Rodwell <ryanrodwell@gmail.com>
2017-07-17 22:14:19 -0400

    adding video pages

--- controller/routes.js
+++ controller/routes.js
@@ -13,5 +13,32 @@ router.get("/videos/advancedtrivia", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/videos/advancedtrivia.html"));
 });
 
+router.get("/videos/psychicgame", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/videos/psychicgame.html"));
+});
+
+router.get("/videos/illegalformation", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/videos/illegalformation.html"));
+});
+
+router.get("/videos/stylecast", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/videos/stylecast.html"));
+});
+
+router.get("/videos/liri", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/videos/liri.html"));
+});
+
+router.get("/videos/flashcards", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/videos/flashcards.html"));
+});
+
+router.get("/videos/trainschedule", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/videos/trainschedule.html"));
+});
+
+router.get("/videos/burger", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/videos/burger.html"));
+});
 
 module.exports = router;
